760b1d147798dd8d6f549cfe2677a2b914a822a7
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2024-04-10 23:05:29 +0800

    Title: 함수를 이용한 합과 소수 판별, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240411/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean sosu(int n){
+        for(int i = 2; i<n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean addEven(int n){
+        int frontNum = n/10;
+        int backNum = n %10;
+        if((frontNum+backNum)%2 != 0){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int cnt = 0;
+        for (int i = a; i <= b; i++){
+            if(sosu(i) && addEven(i)){
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
