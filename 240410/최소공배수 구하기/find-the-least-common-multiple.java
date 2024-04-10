c5436e040aabe3ac7ffc479d49bbfc4899a702c6
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2024-04-10 18:30:44 +0800

    Title: 최소공배수 구하기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240410/최소공배수 구하기/find-the-least-common-multiple.java
+++ 240410/최소공배수 구하기/find-the-least-common-multiple.java
@@ -6,16 +6,19 @@ public class Main {
         int nNum = sc.nextInt();
         int mNum = sc.nextInt();
 
-        LCD(nNum, mNum);
+        int gcd = GCD(nNum, mNum);
+        int lcm = (nNum * mNum) / gcd;
+
+        System.out.print(lcm);
     }
-    public static void LCD(int n, int m){
-        int max = 1;
-        for(int i=1; i<n && i<m; i++){
-            if(n % i == 0 && m % i ==0){
-                max = i;
+    public static int GCD(int n, int m){
+        int gcd = 1;
+
+        for(int i=1; i<=Math.min(n,m); i++){
+            if(n % i ==0 && m % i ==0){
+                gcd = i;
             }
         }
-        int min = (n * m) / max;
-        System.out.print(min);
+        return gcd;
     }
 }
