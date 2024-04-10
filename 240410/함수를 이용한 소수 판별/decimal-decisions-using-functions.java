90fb7494cdee06692c7ac04faf8dfb4619e74de1
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2024-04-10 19:56:55 +0800

    Title: 함수를 이용한 소수 판별, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240410/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240410/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -16,6 +16,9 @@ public class Main {
         System.out.print(sum);
     }
     public static boolean isPrime(int n){
+        if(n <2){
+            return false;
+        }
         for (int i=2; i<n; i++){
             if(n % i == 0){
                 return false;
