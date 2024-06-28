e5c84869809c9033d0c1be0a710093e116fbd762
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2024-06-29 00:22:38 +0900

    Title: 공약수의 유무, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240629/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240629/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,10 +1,14 @@
 a,b = input().split()
 a =int(a)
 b = int(b)
+GCN = False
 
 for i in range(a, b+1):
     if(1920%i==0 and 2880%i==0):
-        print(1)
+        GCN = True
         break
-    else:
-        print(0)
+
+if(GCN):
+    print(1)
+else:
+    print(0)
