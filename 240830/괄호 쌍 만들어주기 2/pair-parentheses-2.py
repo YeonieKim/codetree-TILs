b9ea7da217a4578526d1bfea341294c27a693677
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2024-08-30 15:11:34 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240830/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240830/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,12 +1,9 @@
 arr = list(input())
 #print(arr)
-o = 0
-c = 0
-for i in range(len(arr)-1):
-    if(arr[i] == "(" and arr[i+1] =="("):
-        o += 1
-    if(arr[i]==")" and arr[i+1]==")"):
-        c += 1
+cnt =0
+for i in range(len(arr)-3):
+    for j in range(i+2, len(arr)-1):
+        if(arr[i] == "(" and arr[i+1] =="(" and arr[j]==")" and arr[j+1]==")"):
+            cnt +=1
 
-pair_num = o * c
-print(pair_num)
+print(cnt)
