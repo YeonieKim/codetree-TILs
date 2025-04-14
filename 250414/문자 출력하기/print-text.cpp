2a8559f35d6bec4a49f064d41afe13f9a9d15d3b
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2025-04-14 20:18:34 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250414/문자 출력하기/print-text.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char k;
+    cin >> k;
+    for (int i=0;i<8;i++){
+        cout << k;
+    }
+    return 0;
+}
