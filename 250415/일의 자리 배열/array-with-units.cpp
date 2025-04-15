ec1f356efa998526b71ca831fd6969629d5446f6
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2025-04-15 11:02:25 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250415/일의 자리 배열/array-with-units.cpp
+++ 250415/일의 자리 배열/array-with-units.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int a, b, c;
+    int a, b, c,m;
     cin >> a >> b;
 
     cout << a << " " << b << " ";
-    for (int i =0; i<10; i++){
-        // cout << (a + b)%10 << " ";
+    for (int i =0; i<8; i++){
+        c= (a+b)%10;
+        cout << c << " ";
+        m = a;
         a = b;
-        b = (a + b)%10;
+        b = c;
     }
     return 0;
 }
