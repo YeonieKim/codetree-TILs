7de526bcbecb57f4e17968593becd55ac3b4c0fc
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2025-04-16 11:19:59 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250416/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i;
+    string a;
+    while (true){
+        cin >> a;
+
+        if(a == "0"){
+            break;
+        }
+        cout << a << "\n";
+    }
+    return 0;
+}
