aeefe3b2cca5c8baee7fdb2b87e2c0a3216b481e
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2025-04-16 10:40:20 +0900

    Title: 친근하지 않은 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250416/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    int num = 0;
+    cin >> N;
+
+    for (int i=1; i<=N; i++){
+        if(i%2==0 || i%3==0 || i%5==0){
+            continue;
+        }
+        num += 1;
+    }
+    cout << num;
+
+    return 0;
+}
