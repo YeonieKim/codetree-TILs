e6b58a608a14f932d7a230bdf8dfa13a97602cd7
YeonieKim <141324304+YeonieKim@users.noreply.github.com>
2025-04-18 11:37:33 +0900

    Title: 약수가 세 개인 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250418/약수가 세 개인 수/numbers-with-three-divisors.cpp
+++ 250418/약수가 세 개인 수/numbers-with-three-divisors.cpp
@@ -8,15 +8,21 @@ int main() {
     cin >> s >> e;
 
     // Please write your code here.
-    int cnt =0;
+    int cnt = 0;
+    int d = 0;
     
     for(int i=s; i<=e; i++){
+        if(cnt == 3){
+            d++;
+        }
+        cnt = 0;
         for(int j=1; j<=i; j++){
             if(i % j == 0){
                 cnt++;
             }
         }
     }
-    cout << cnt;
+    
+    cout << d;
     return 0;
 }
